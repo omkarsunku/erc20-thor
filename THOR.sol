b34a538093f8f488fdc471a4c8dfd51339ece8c2
omkarsunku <34002297+omkarsunku@users.noreply.github.com>
2021-09-30 19:03:42 +0530

    added erc20-thor

---
+++ THOR.sol
@@ -0,0 +1,113 @@
+// contracts/MyToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Thor {
+    string public name = "Thoritos";
+    string public symbol = "THOR";
+    uint256 public totalSupply = 100000;
+    uint8 public decimals = 8;
+    address owner;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    struct TimeLock {
+        uint256 _amount;
+        uint256 _releaseTime;
+    }
+
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) private allowance;
+    mapping(address => TimeLock) lockData;
+
+    constructor() {
+        owner = msg.sender;
+        balances[owner] = totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0), "Approve ERC20 to zero address");
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balances[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function mint(uint256 amount) internal {
+        require(msg.sender != owner, "Only Owner can call mint");
+        totalSupply += amount;
+        balances[owner] += amount;
+        emit Transfer(address(0), owner, amount);
+    }
+
+    function burn(uint256 amount) internal {
+        require(msg.sender != owner, "Only Owner can call burn");
+        uint256 accountBalance = balances[owner];
+        require(accountBalance < amount, "Burning amount exceeds balance");
+        balances[owner] = accountBalance - amount;
+        accountBalance = 0;
+        totalSupply -= amount;
+        emit Transfer(owner, address(0), amount);
+    }
+
+    function lock(uint256 releaseTime, uint256 amount) public {
+        require(
+            amount > balances[msg.sender],
+            "Amount can't be greater than balance"
+        );
+        require(
+            releaseTime < block.timestamp,
+            "Release time must be greater than current timestamp"
+        );
+        lockData[msg.sender] = TimeLock(amount, releaseTime);
+        balances[msg.sender] -= amount;
+    }
+
+    function release() public {
+        require(
+            lockData[msg.sender]._releaseTime < block.timestamp,
+            "Time to release not yet reached"
+        );
+        uint256 amount = lockData[msg.sender]._amount;
+        lockData[msg.sender]._amount = 0;
+        lockData[msg.sender]._releaseTime = 0;
+        balances[msg.sender] += amount;
+    }
+}
